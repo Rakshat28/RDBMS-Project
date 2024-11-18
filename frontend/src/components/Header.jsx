7a9a636e0f4b183c296223441d7e077821af4801
Rakshat28 <rakshat07ps@gmail.com>
2024-11-18 23:34:28 +0530

    log out feature added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -187,7 +187,7 @@ export default function Header() {
                 </a>
                 
               </div>
-              <div className="py-6">
+              <div className="py-6" onClick={handleLogout}>
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-red-200"
